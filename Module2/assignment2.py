d65a054389cf5ec752517513d0913b737859bec3
Unknown <patrick.stahl@gmail.com>
2016-10-28 16:37:07 +0300

    Assignment 3 Finished

--- Module2/assignment2.py
+++ Module2/assignment2.py
@@ -3,13 +3,13 @@ import pandas as pd
 # TODO: Load up the 'tutorial.csv' dataset
 #
 # .. your code here ..
-
-
+df = pd.read_csv('C:/Data/Projektit/DAT210x/Module2/Datasets/tutorial.csv')
+print df
 
 # TODO: Print the results of the .describe() method
 #
 # .. your code here ..
-
+print df.describe()
 
 
 # TODO: Figure out which indexing method you need to
@@ -17,4 +17,4 @@ import pandas as pd
 # And print the results
 #
 # .. your code here ..
-
+df.loc[2:4,'col3']
--- Module2/assignment3.py
+++ Module2/assignment3.py
@@ -4,13 +4,18 @@ import pandas as pd
 # Ensuring you set the appropriate header column names
 #
 # .. your code here ..
-
+names = ['motor', 'screw', 'pgain', 'vgain', 'class']
+df = pd.read_csv('c:/Data/Projektit/DAT210x/Module2/Datasets/servo.data', names=names )
+print df.head()
 
 # TODO: Create a slice that contains all entries
 # having a vgain equal to 5. Then print the 
 # length of (# of samples in) that slice:
 #
 # .. your code here ..
+entriesWith5vgain=df[df['vgain']==5]
+print "entries having a vgain equal to 5: " + str( len(entriesWith5vgain))
+
 
 
 # TODO: Create a slice that contains all entries
@@ -19,8 +24,8 @@ import pandas as pd
 # samples in) that slice:
 #
 # .. your code here ..
-
-
+entriesWithMotorandScrewE=df[(df['motor']=='E') & (df['screw']=='E')]
+print "Entries having a motor equal to E and screw equal to E: "+ str(len(entriesWithMotorandScrewE))
 
 # TODO: Create a slice that contains all entries
 # having a pgain equal to 4. Use one of the
@@ -30,10 +35,11 @@ import pandas as pd
 #
 # .. your code here ..
 
-
+print "Mean vgain of Entries with pgain=4"
+print df[df['pgain']==4]['vgain'].mean()
 
 # TODO: (Bonus) See what happens when you run
 # the .dtypes method on your dataframe!
 
-
+df.dtypes
 
